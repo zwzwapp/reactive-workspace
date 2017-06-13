37c2d10bc9f66b88e1ca39656112098b247dc96b
Ngal <Ngal@Thuzar-PC>
2017-06-13 11:46:05 +0100

    create discovery demo

---
+++ src/main/java/com/main/discovery/DiscoveryDemoApplication.java
@@ -0,0 +1,14 @@
+package com.main.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryDemoApplication.class, args);
+	}
+}
